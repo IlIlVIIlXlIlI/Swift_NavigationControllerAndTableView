1ec59fbc7400c7dd609e1ecb8c756352ba4b9937
Shogo <vanitas710@yahoo.co.jp>
2021-01-13 01:11:03 +0900

    Initial Commit

---
+++ NavigationControllerAndTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NavigationControllerAndTableView
+//
+//  Created by Shogo Nobuhara on 2021/01/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
